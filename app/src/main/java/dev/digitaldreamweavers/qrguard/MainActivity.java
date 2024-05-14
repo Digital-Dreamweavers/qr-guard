56797e87dc89c9fb8d40337928ccdd315d230aee
Meowcino <meow@catboy.au>
2024-05-14 13:40:53 +1000

    ViewModel implemented

--- app/src/main/java/dev/digitaldreamweavers/qrguard/MainActivity.java
+++ app/src/main/java/dev/digitaldreamweavers/qrguard/MainActivity.java
@@ -3,17 +3,32 @@ package dev.digitaldreamweavers.qrguard;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
 import dev.digitaldreamweavers.qrguard.databinding.ActivityMainBinding;
+import dev.digitaldreamweavers.qrguard.ui.ViewFinderFragment;
+import dev.digitaldreamweavers.qrguard.ui.ViewFinderViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +37,23 @@ public class MainActivity extends AppCompatActivity {
     private PreviewView qrViewFinder;
     private ExecutorService executor;
     private ImageCapture imageCapture;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ViewFinderViewModel viewFinderModel;
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        qrViewFinder = findViewById(R.id.viewFinder);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.cameraContainer,
+                        new ViewFinderFragment())
+                .commit();
+
+        viewFinderModel = new ViewModelProvider(this).get(ViewFinderViewModel.class);
 
         // get camera perms, not proper implementation
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -43,13 +67,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                //.requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(qrViewFinder.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+    }
+
 
     private void startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        // Bind camera to listener.
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Log.w(TAG, "Could not add Camera Listener: ", e);
+            }
+            }, ContextCompat.getMainExecutor(this));
+
         // Create a Toast object
         Toast toast = Toast.makeText(this, "Camera starts here.", Toast.LENGTH_SHORT);
 
         // Show the Toast
         toast.show();
     }
+
+
 
 }
