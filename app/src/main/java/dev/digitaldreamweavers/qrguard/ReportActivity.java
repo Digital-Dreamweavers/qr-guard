484ea4fdc553bb3f621ff369256918fb8dfd5538
Meowcino <meow@catboy.au>
2024-05-23 23:26:29 +1000

    Navigation Icons, Block Nav reselections

--- app/src/main/java/dev/digitaldreamweavers/qrguard/ReportActivity.java
+++ app/src/main/java/dev/digitaldreamweavers/qrguard/ReportActivity.java
@@ -24,19 +24,5 @@ public class ReportActivity extends AppCompatActivity {
         binding = ActivityReportBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
     }
 }
--- app/src/main/java/dev/digitaldreamweavers/qrguard/ui/BottomNavigationFragment.java
+++ app/src/main/java/dev/digitaldreamweavers/qrguard/ui/BottomNavigationFragment.java
@@ -1,29 +1,20 @@
 package dev.digitaldreamweavers.qrguard.ui;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseUser;
 
 import dev.digitaldreamweavers.qrguard.R;
@@ -51,15 +42,12 @@ public class BottomNavigationFragment extends Fragment {
         if (id == R.id.navigation_camera) {
             Log.i(TAG, "Camera selected.");
             fragment = new ViewFinderFragment();
-            // TODO: Camera
         } else if (id == R.id.navigation_profile) {
             Log.i(TAG, "Profile selected.");
             fragment = new ProfileFragment();
-            // TODO: Profile
         } else if (id == R.id.navigation_map) {
             Log.i(TAG, "Map selected.");
             fragment = new MapFragment();
-            // TODO: Map
         }
 
         // Replace the current fragment with the selected fragment.
@@ -71,6 +59,10 @@ public class BottomNavigationFragment extends Fragment {
         Log.w(TAG, id + " does not fit into following IDs: " + R.id.navigation_camera + ", " + R.id.navigation_profile + ", " + R.id.navigation_map);
 
         return false;
+    };
+
+    private final NavigationBarView.OnItemReselectedListener onItemReselected = item -> {
+        Log.i(TAG, "Item reselected: " + item.getItemId() + ". Discarding action.");
     };
 
     @Override
@@ -107,6 +99,9 @@ public class BottomNavigationFragment extends Fragment {
         Log.i(TAG, "Binding BottomNavigationView to listener.");
         BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(onItemSelected);
+        bottomNavigationView.setOnItemReselectedListener(onItemReselected);
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_camera);
 
         return view;
     }
